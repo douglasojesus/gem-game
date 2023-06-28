18b2907b052162a23b17712e21fc4dc112915485
Douglas de Jesus <dougecomp@gmail.com>
2023-06-28 17:40:07 -0300

    :sparkles: feat: Permutação de gemas

--- src/GameFunctions.py
+++ src/GameFunctions.py
@@ -35,7 +35,6 @@ def verificaCadeia(pontuacao, tabuleiro, linhas, colunas):
     continuaVerificacaoDeCadeia = True
     #O tabuleiro vai continuar sendo verificado até não haver mais destruição de gemas
     while continuaVerificacaoDeCadeia:
-        print("tal rodada")
         continuaVerificacaoDeCadeia = False
         for i in range(linhas):
             for j in range(colunas):
@@ -74,15 +73,46 @@ def verificaCadeia(pontuacao, tabuleiro, linhas, colunas):
                 if (destruiuGemasV or destruiuGemasH):
                     continuaVerificacaoDeCadeia = True
 
-def permutaPosicao(tabuleiro, linha1, coluna1, linha2, coluna2):
+#Linha e coluna -> referente a posição da primeira gema relacionada.
+#Gema -> a segunda gema a ser relacionada.
+def verificaCadeiaPorPermutacao(tabuleiro, linha, coluna, gema, linhas, colunas):
+    #Verifica se após permutar, irá gerar uma cadeia
+    gemasIguaisVerticais = gemasIguaisHorizontais = 1 #A gema atual é igual a ela mesma
+    #Verifica se permutar a gema 1 pela gema 2, a gema de cima da gema 2 é equivalente
+    aux = linha
+    while (aux > 0 and tabuleiro[aux-1][coluna] == gema):
+        gemasIguaisVerticais += 1
+        aux -= 1
+    #Verifica se permutar a gema 1 pela gema 2, a gema debaixo da gema 2 é equivalente
+    aux = linha
+    while (aux < linhas-1 and tabuleiro[aux+1][coluna] == gema):
+        gemasIguaisVerticais += 1
+        aux += 1
+    #Verifica se permutar a gema 1 pela gema 2, a gema do lado esquerdo da gema 2 é equivalente
+    aux = coluna
+    while (aux > 0 and tabuleiro[linha][aux-1] == gema):
+        gemasIguaisHorizontais += 1
+        aux -= 1
+    #Verifica se permutar a gema 1 pela gema 2, a gema do lado direito da gema 2 é equivalente
+    aux = coluna
+    while (aux < colunas-1 and tabuleiro[linha][aux+1] == gema):
+        gemasIguaisHorizontais += 1
+        aux += 1
+    if (gemasIguaisVerticais >= 3 or gemasIguaisHorizontais >= 3):
+        return True
+    return False
+
+def permutaPosicao(pontuacao, tabuleiro, linha1, coluna1, linha2, coluna2, linhas, colunas):
     #Verifica se a permutação é possível
     if (linha1 == linha2+1 or linha2 == linha1+1 or linha1 == linha2):
         if (coluna1 == coluna2+1 or coluna2 == coluna1+1 or coluna1 == coluna2):
             if (tabuleiro[linha1][coluna1] != tabuleiro[linha2][coluna2]):
-                if True: #verificaCadeia(tabuleiro, linha1, coluna1, linha2, coluna2):
+                if (verificaCadeiaPorPermutacao(tabuleiro, linha1, coluna1, tabuleiro[linha2][coluna2], linhas, colunas) or 
+                    verificaCadeiaPorPermutacao(tabuleiro, linha2, coluna2, tabuleiro[linha1][coluna1], linhas, colunas)):
                     buffer = tabuleiro[linha1][coluna1]
                     tabuleiro[linha1][coluna1] = tabuleiro[linha2][coluna2]
                     tabuleiro[linha2][coluna2] = buffer
+                    verificaCadeia(pontuacao, tabuleiro, linhas, colunas)
                     print("Permutado")
                     return True
     print("Não foi possível permutar")
